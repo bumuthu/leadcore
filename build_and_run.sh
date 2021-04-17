e3ff12ae3a4884fe87be9862a5c2490c1b54fb4f
bumuthu <bumuthu.dilshan@gmail.com>
2021-04-17 14:29:43 +0530

    build and run

---
+++ build_and_run.sh
@@ -0,0 +1,10 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+cd ./scraper
+docker build -t leadcore/scraper .
+cd ../backend
+docker build -t leadcore/api .
+cd ..
+
+docker-compose -d up
